39dcf523787d5edad4ec68f8a43d5684d02a7b04
Your Name <ebrahim.karjatwala@stud.uni-due.de>
2021-09-25 19:07:34 +0200

    we are commiting the HAUPTPROJECT on the main branch

---
+++ src/com/github/Main.java
@@ -0,0 +1,9 @@
+package com.github;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ProjectClass pc= new ProjectClass();
+        pc.projectMethod();
+    }
+}
---
+++ src/com/github/ProjectClass.java
@@ -0,0 +1,8 @@
+package com.github;
+
+public class ProjectClass {
+
+    public void projectMethod(){
+        System.out.println("this is our main project!!!");
+    }
+}
